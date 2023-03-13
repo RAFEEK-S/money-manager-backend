17152e395b7eb303c90c578d58e565981130390e
RAFEEK-S <115687702+RAFEEK-S@users.noreply.github.com>
2023-03-13 10:49:34 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -20,7 +20,7 @@ var corsOptions = {
     origin: "http://localhost:3000"
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
